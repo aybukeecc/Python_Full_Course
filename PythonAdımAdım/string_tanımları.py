517b721194fd40fb3eecec93265725dbd7b94f60
ertugrull.gns <74158698+ertugrul37@users.noreply.github.com>
2023-11-23 22:45:20 +0400

    update

--- PythonAdımAdım/string_tanımları.py
+++ PythonAdımAdım/string_tanımları.py
@@ -1,9 +1,24 @@
 firstName = "ertugrul"
 lastName =  "güneş"
-age =  "23"
 
-firstNameUpper = firstName.upper() # firstName içindeki string veriyi tüm küçük harflerini büyük harflere çeviriyor.
+age = 23
+age += 1
+# ---------------------------------------------------------------------------------------------------------------------------
 
-print(" HOŞGELDİN "+ firstNameUpper +"\n") # çevrilen büyük yazıyı ekrana yazdırıyoruz.
+#firstNameUpper = firstName.upper() # firstName içindeki string veriyi tüm küçük harflerini büyük harflere çeviriyor.
+#print(" HOŞGELDİN "+ firstNameUpper +"\n") # çevrilen büyük yazıyı ekrana yazdırıyoruz.
+#print("firstname = " +firstName + "\n" +"lastname = "+ lastName +"\n"+ "age = "+ age) # string olarak tanımlanan verileri konsol ekranına yazdırıyoruz.
 
-print("firstname = " +firstName + "\n" +"lastname = "+ lastName +"\n"+ "age = "+ age) # string olarak tanımlanan verileri konsol ekranına yazdırıyoruz.
+# ---------------------------------------------------------------------------------------------------------------------------
+
+#print("yaşınız : " + str(age)) # "str" integer olmadan yazarask type error hatası alırız onun için integer veriyi str ile veriyi string veriye çeviyoruz.
+
+# ---------------------------------------------------------------------------------------------------------------------------
+
+#height = 250.5
+#widht = 300
+#a = height * widht # alan hesaplama formülü
+#print(a)
+#print(type(a)) # type(yazılıcak değer) bu tanım çıktının türünü gösterir.
+
+# ---------------------------------------------------------------------------------------------------------------------------
