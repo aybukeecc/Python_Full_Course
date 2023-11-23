c6d847a4205a8491d6193ffb1142e2e7e9a1d469
ertugrull.gns <74158698+ertugrul37@users.noreply.github.com>
2023-11-23 23:03:11 +0400

    update

--- PythonAdımAdım/Tanımlar.py
+++ PythonAdımAdım/Tanımlar.py
@@ -3,22 +3,26 @@ lastName =  "güneş"
 
 age = 23
 age += 1
-# ---------------------------------------------------------------------------------------------------------------------------
+# ----------------------------------STRİNG TANIM-GİRDİLERİ BÜYÜTME--------------------------------------------------------------------------
 
 #firstNameUpper = firstName.upper() # firstName içindeki string veriyi tüm küçük harflerini büyük harflere çeviriyor.
 #print(" HOŞGELDİN "+ firstNameUpper +"\n") # çevrilen büyük yazıyı ekrana yazdırıyoruz.
 #print("firstname = " +firstName + "\n" +"lastname = "+ lastName +"\n"+ "age = "+ age) # string olarak tanımlanan verileri konsol ekranına yazdırıyoruz.
 
-# ---------------------------------------------------------------------------------------------------------------------------
+# --------------------------------STR-------------------------------------------------------------------------------------------
 
 #print("yaşınız : " + str(age)) # "str" integer olmadan yazarask type error hatası alırız onun için integer veriyi str ile veriyi string veriye çeviyoruz.
 
-# ---------------------------------------------------------------------------------------------------------------------------
+# --------------------------------FLOAT-------------------------------------------------------------------------------------------
 
-#height = 250.5
-#widht = 300
+#height = 250.5 #float bir değerdir
+#widht = 300.5 #float bir değerdir
 #a = height * widht # alan hesaplama formülü
 #print(a)
 #print(type(a)) # type(yazılıcak değer) bu tanım çıktının türünü gösterir.
 
-# ---------------------------------------------------------------------------------------------------------------------------
+# ---------------------------------BOOLEAN------------------------------------------------------------------------------------------
+
+dog = True
+cat = False
+print( "DOG :"+str(dog)+"\n"+ "CAT :"+ str(cat))
